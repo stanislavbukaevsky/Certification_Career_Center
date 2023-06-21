2fd5c06c39ce3a7dfcabb7eb84cb3614f3400ee4
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-06-21 14:07:51 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/certificationcareercenter/CertificationCareerCenterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.certificationcareercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationCareerCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationCareerCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/certificationcareercenter/CertificationCareerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.certificationcareercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationCareerCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
